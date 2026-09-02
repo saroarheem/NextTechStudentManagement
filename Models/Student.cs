fe0e5c2bdeb854d829c6f245fa799f8e217ae4ce
saroarheem <saroarheem@gmail.com>
2026-09-02 11:04:09 +0600

    Added Student model

---
+++ Models/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace NextTechStudentManagement.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string StudentId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string Department { get; set; }
+        public string Course { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}

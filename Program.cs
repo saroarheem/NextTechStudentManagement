ca77933c00729adece567f03a379e7d179eb3759
saroarheem <saroarheem@gmail.com>
2026-09-02 12:05:05 +0600

    Changed Default View To Student

--- Program.cs
+++ Program.cs
@@ -30,7 +30,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Student}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
